045badfd37808c981a0a7a3d578e02fedc461a11
José Luis Puc <jose.luis@grupoicarus.com.mx>
2017-06-08 16:54:23 -0500

    seguimos

--- kazumy.js
+++ kazumy.js
@@ -8,14 +8,19 @@ Kazumy.prototype.viewText = function (id, data) {
 
 Kazumy.prototype.dataView = function (id, dataJSON) {
   var content = $(id).text();
-  arrayString = content.split(" ")
+  var arrayString = content.split(" ")
 
-  arrayString.forEach(function (item, index) {
-    element = item.substring(1);
-    remplazo = '%'+element
-    for (var key in dataJSON) {
-      alert(dataJSON[key]);
-      content = content.replace(remplazo, dataJSON[key])
+  arrayString.forEach( function (item, index) {
+    if(item != "" && item != " "){
+      var element = item.substring(1);
+alert(element)
+      //for (var key in dataJSON) {
+        if(element != " " ){
+          remplazo = '%'+element
+          console.log(element)
+          content = content.replace(remplazo, dataJSON[element])
+        }  
+      //}
     }
   });
 
