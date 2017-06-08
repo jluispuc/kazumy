35d7933bc2be026354f780937e58a315ea5ed9a3
jluispuc <j.louis1493@gmail.com>
2017-06-08 07:55:10 -0500

    un método más

--- kazumy.js
+++ kazumy.js
@@ -5,3 +5,19 @@ function Kazumy(){
 Kazumy.prototype.viewText = function (id, data) {
   $(id).append('<p>'+data+'</p>');
 };
+
+Kazumy.prototype.dataView = function (id, dataJSON) {
+  var content = $(id).text();
+  arrayString = content.split(" ")
+
+  arrayString.forEach(function (item, index) {
+    element = item.substring(1);
+    remplazo = '%'+element
+    for (var key in dataJSON) {
+      alert(dataJSON[key]);
+      content = content.replace(remplazo, dataJSON[key])
+    }
+  });
+
+  $(id).text(content);
+};
