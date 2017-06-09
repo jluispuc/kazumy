f72744fda550d2ed220db8adae74cda35b515db0
José Luis Puc <jose.luis@grupoicarus.com.mx>
2017-06-09 10:30:26 -0500

    no menssage

--- kazumy.js
+++ kazumy.js
@@ -4,23 +4,23 @@ function Kazumy(){
 
 Kazumy.prototype.textView = function (id, data) {
   $(id).append('<p>'+data+'</p>');
-};
+}
 
-Kazumy.prototype.dataView = function (id, dataJSON) {
+Kazumy.prototype.dataView = function (id, arrayData) {
   var content = $(id).text();
-  content = content.trim()
-  var arrayString = content.split(" ")
+  content = content.trim();
+  var arrayString = content.split(" ");
 
   arrayString.forEach( function (item, index) {
-      var cadena = item
-      element = cadena.replace(/[.,;:]/gi, "")
+      var cadena = item;
+      element = cadena.replace(/[.,;:]/gi, "");
       element = element.substring(1, element.length-1);
 
-      if(dataJSON[element.toString()] != undefined){
-        remplazo = '['+element+']'
-        content = content.replace(remplazo, dataJSON[element.toString()])
+      if(arrayData[element.toString()] != undefined){
+        remplazo = '['+element+']';
+        content = content.replace(remplazo, arrayData[element.toString()]);
       }
   });
 
   $(id).text(content);
-};
+}
